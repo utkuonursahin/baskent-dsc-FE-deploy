712935e1a10d87ad465d952335fe95d2e410d932
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-23 18:49:02 +0300

    contact bug fix

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,10 +7,10 @@ const Contact = () => {
       <h2 className="heading-2">İletişim</h2>
       <p>Sosyal medya hesaplarımızı takip edebilir, aşağıdaki buton ile direkt olarak bize ulaşabilirsin!</p>
       <div className="contact__socials">
-        <img src="https://www.linkedin.com/company/developer-students-community/" alt="linkedin icon"/>
-        <img src="https://www.instagram.com/baskentdsc/" alt="instagram icon"/>
-        <img src="https://chat.whatsapp.com/FDtofP4pVxgBmbiaCw6BXr" alt="whatsapp icon"/>
-        <img src="https://discord.gg/wBQnWdUw" alt="discord icon"/>
+        <Link href="https://www.linkedin.com/company/developer-students-community/"><img src="/linkedin.svg" alt="linkedin icon"/></Link>
+        <Link href="https://www.instagram.com/baskentdsc/"><img src="/instagram.svg" alt="instagram icon"/></Link>
+        <Link href="https://chat.whatsapp.com/FDtofP4pVxgBmbiaCw6BXr"><img src="/whatsapp.svg" alt="whatsapp icon"/></Link>
+        <Link href="https://discord.gg/wBQnWdU"><img src="/discord.svg" alt="discord icon"/></Link>
       </div>
       <button className="btn btn-cta"><Link href="https://docs.google.com/forms/d/e/1FAIpQLSf_oiFNow0vfISCxanxsqQeaQdzcLqihYpYT9CdfqoQyekXXg/viewform?usp=sf_link" target="_blank">Bİze Ulaş</Link></button>
     </section>
