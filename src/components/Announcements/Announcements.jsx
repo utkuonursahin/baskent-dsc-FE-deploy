27e48ed1bb45780c3fd4b519eb76e046cd249793
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-27 21:30:16 +0300

    socials z-index bug fix

--- src/components/Announcements/Announcements.jsx
+++ src/components/Announcements/Announcements.jsx
@@ -14,7 +14,7 @@ const Announcements = () => {
         if(!response.results) return
         const newAnnouncements = response?.data?.data
         if(newAnnouncements.length) setAnnouncements([...announcements,...newAnnouncements])
-      }catch(error){setError(error.response?.data.message)}
+      }catch(error){setError(error.response?.data.message || error.response.data)}
     })();
   },[page, setError, setAnnouncements]);
   return (
