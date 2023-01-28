53e03e1425f8d0e7d84ea94757fb82aa34aa519c
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-28 13:40:14 +0300

    mobile friendly images

--- src/components/Executives/SliderContent/SliderContent.js
+++ src/components/Executives/SliderContent/SliderContent.js
@@ -6,7 +6,13 @@ function SliderContent({data}) {
   }
   return (
     <div className="slider-content">
-      <Image width={100} height={100} src={`${data.photo}`} loader={loadImg} alt="Executive Photo" crossorigin="anonymous" className="slider-content__image"/>
+      <Image width={100} height={100} src={`${data.photo}`}
+             loader={loadImg} alt="Executive Photo" crossorigin="anonymous"
+             className="slider-content__image"
+             sizes="(max-width: 640px) 50vw,
+             (max-width: 768px) 30vw,
+             (max-width: 1024px) 30%,
+             (max-width: 1440px) 23rem"/>
       <div>
         <h4 className="heading-4">{data.name}</h4>
         <span className="slider-content__position">{data.title}</span>
