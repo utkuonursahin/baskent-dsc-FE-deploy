3f2c745e140bd5f73264ca0336d80ab1ea7d33a6
utkuonursahin <sahinutkuonur@gmail.com>
2023-01-31 00:18:06 +0300

    font change from open sans to poppins

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Rubik&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Rubik:wght@400;500;600&display=swap" rel="stylesheet"/>
       </Head>
       <body>
       <Main />
