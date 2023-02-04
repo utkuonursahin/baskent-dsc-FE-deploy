81429f0a66b1c19e7d115eb2e9838eaef7e34f32
utkuonursahin <sahinutkuonur@gmail.com>
2023-02-05 00:41:56 +0300

    security update

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
   productionBrowserSourceMaps: false,
+  async headers(){
+    return [{source:'/:path*',headers:[
+      {key:'X-Frame-Options',value:'DENY'},
+        {key:'X-XSS-Protection',value:'1; mode=block'}
+      ]}]
+  }
 }
 module.exports = nextConfig
