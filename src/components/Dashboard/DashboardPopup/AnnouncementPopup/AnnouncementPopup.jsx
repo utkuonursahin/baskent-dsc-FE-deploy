7027645cd6dc584878646531a2c0025048a3770a
utkuonursahin <sahinutkuonur@gmail.com>
2023-02-12 11:22:21 +0300

    announcements date selection improved

--- src/components/Dashboard/DashboardPopup/AnnouncementPopup/AnnouncementPopup.jsx
+++ src/components/Dashboard/DashboardPopup/AnnouncementPopup/AnnouncementPopup.jsx
@@ -13,7 +13,7 @@ const AnnouncementPopup = ({mode, setIsOpened}) => {
       case 'name': return {...state, name: action.value};
       case 'description': return {...state, description: action.value};
       case 'summary': return {...state, summary: action.value};
-      case 'date': return {...state, date: action.value};
+      case 'date': return {...state, date: action.value.toLocaleString().slice(0,10)};
       case 'image': return {...state, image: action.value};
       case 'link': return {...state, link: action.value};
       default: return state;
@@ -25,7 +25,7 @@ const AnnouncementPopup = ({mode, setIsOpened}) => {
         name: currentAnnouncement?.name,
         description: currentAnnouncement?.description,
         summary: currentAnnouncement?.summary,
-        date: currentAnnouncement?.date,
+        date: currentAnnouncement?.date.toLocaleString().slice(0,10),
         image: currentAnnouncement?.imageCover,
         link: currentAnnouncement?.link,
       }
@@ -102,7 +102,7 @@ const AnnouncementPopup = ({mode, setIsOpened}) => {
         </label>
         <label htmlFor="date">
           <span>Date</span>
-          <input type="date" id="date" required={true} value={state.date.toLocaleString().slice(0,10)} onChange={(e)=>dispatch({type:"date",value:e.target.value})}/>
+          <input type="date" id="date" required={true} value={state.date} onChange={(e)=>dispatch({type:"date",value:e.target.value})}/>
         </label>
         <label htmlFor="link">
           <span>Link</span>
